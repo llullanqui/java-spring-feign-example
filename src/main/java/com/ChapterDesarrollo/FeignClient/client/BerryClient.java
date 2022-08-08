30c6e679121a7800ebf660eea675c6cb40ce94c8
llullanqui <luis.1142@hotmail.com>
2022-08-08 16:10:21 -0500

    Basic Logging Configuration

--- src/main/java/com/ChapterDesarrollo/FeignClient/client/BerryClient.java
+++ src/main/java/com/ChapterDesarrollo/FeignClient/client/BerryClient.java
@@ -1,11 +1,12 @@
 package com.ChapterDesarrollo.FeignClient.client;
 
+import com.ChapterDesarrollo.FeignClient.configuration.FeignClientsFullConfiguration;
 import com.ChapterDesarrollo.FeignClient.dto.berry.Berry;
 import com.ChapterDesarrollo.FeignClient.dto.berry.BerryList;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(value = "berries", url = "https://pokeapi.co/api/v2")
+@FeignClient(value = "berries", url = "https://pokeapi.co/api/v2", configuration = FeignClientsFullConfiguration.class)
 public interface BerryClient {
 
     @GetMapping(value = "/berry/{name}")
